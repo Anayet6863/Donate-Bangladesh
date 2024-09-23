5f3c574444c9177390b47b2a81be13f154b9b037
Md Anayet Ullah <109759412+Anayet6863@users.noreply.github.com>
2024-09-24 04:56:33 +0600

    All js DOM is added but some functionality are incomplete

--- code/feature.js
+++ code/feature.js
@@ -3,12 +3,21 @@ document.getElementById("donate-btn").addEventListener('click', function () {
     //console.log("frature Loaded");
     hiddenClass("hidden-items");
     addClass("history-container");
+    // Toggle button:
+
+    document.getElementById("donate-btn").classList.add("bg-[#B4F461]");
+    document.getElementById("history-btn").classList.remove("bg-[#B4F461]");
+
 })
 //Call for adding class.
 document.getElementById("history-btn").addEventListener('click', function () {
     //console.log("frature Loaded");
     hiddenClass("history-container");
     addClass("hidden-items");
+    //Toggle button:
+    document.getElementById("history-btn").classList.add("bg-[#B4F461]");
+    document.getElementById("donate-btn").classList.remove('bg-[#B4F461]');
+
 })
 // Calling  blog page:
 document.getElementById("call-blog").addEventListener('click', function () {
@@ -21,19 +30,29 @@ document.getElementById("call-blog").addEventListener('click', function () {
 
 // input call for card1
 document.getElementById("donate-now-btn1").addEventListener('click', function () {
+    
 
     const card1Value = getInputFieldValue("card1-input");
-    if (card1Value>='0' && card1Value<='9') {
-        console.log(parseFloat(card1Value));
+    // if(card1Value === null || !isNaN(card1Value)){
+    //     alert("YES");
+    // }
+    const currentBalance = document.getElementById("current-balance-btn").innerText;
+    if (parseFloat(card1Value) > parseFloat(currentBalance)) {
+        alert("Insuffient Balance! Connot donate.");
+    }
+    else {
+        //console.log(parseFloat(card1Value));
         updateBalance(card1Value);
         // Update Self Card balance:
         const selfCardBalance = document.getElementById("self-card1-btn").innerText;
-        // console.log(selfCardBalance);
         document.getElementById("self-card1-btn").innerText = parseFloat(selfCardBalance) + parseFloat(card1Value);
+        // Transection history added:
+        historyAdded(card1Value, "donated for Flood at Noakhali, Bangladesh");
+        openModal();
     }
-    else {
-        alert("Input is invalid.");
-    }
+
+
+
 
 
 
@@ -43,17 +62,25 @@ document.getElementById("donate-now-btn1").addEventListener('click', function ()
 // Iput call for card2
 document.getElementById("donate-now-btn2").addEventListener('click', function () {
     const card2Value = getInputFieldValue("card2-input");
-    if (!isNaN(card2Value ) && card2Value == null) {
+    const currentBalance = document.getElementById("current-balance-btn").innerText;
+    if (parseFloat(card2Value) > parseFloat(currentBalance)) {
+
+        alert("Insuffient Balance! Connot donate.");
+
+    }
+    else {
+
 
         updateBalance(card2Value);
 
         // Update Self Card balance:
-        const selfCardBalance = document.getElementById("self-card1-btn").innerText;
+        const selfCardBalance = document.getElementById("self-card2-btn").innerText;
         // console.log(selfCardBalance);
         document.getElementById("self-card2-btn").innerText = parseFloat(selfCardBalance) + parseFloat(card2Value);
-    }
-    else {
-        alert("Input is invalid.");
+
+        // Transection history added:
+        historyAdded(card2Value, "donated for Flood Relief in Feni,Bangladesh");
+        openModal();
     }
 
 
@@ -61,7 +88,15 @@ document.getElementById("donate-now-btn2").addEventListener('click', function ()
 // Iput call for card3
 document.getElementById("donate-now-btn3").addEventListener('click', function () {
     const card3Value = getInputFieldValue("card3-input");
-    if (!isNaN(card3Value) && card3Value == null) {
+    const currentBalance = document.getElementById("current-balance-btn").innerText;
+    if (parseFloat(card3Value) > parseFloat(currentBalance)) {
+
+        alert("Insuffient Balance! Connot donate.");
+    }
+    else {
+
+
+
 
         updateBalance(card3Value);
 
@@ -69,9 +104,9 @@ document.getElementById("donate-now-btn3").addEventListener('click', function ()
         const selfCardBalance = document.getElementById("self-card3-btn").innerText;
         // console.log(selfCardBalance);
         document.getElementById("self-card3-btn").innerText = parseFloat(selfCardBalance) + parseFloat(card3Value);
-    }
-    else{
-        alert("Input is invalid.")
+        // History added:
+        historyAdded(card3Value, "donated for Aid for Injured in the Quota Movement, Bangladesh");
+        openModal();
     }
 
 
--- code/utilities.js
+++ code/utilities.js
@@ -1,39 +1,64 @@
 //Function for Remove class
-function hiddenClass(id){
+function hiddenClass(id) {
   document.getElementById(id).classList.remove('hidden');
   //document.getElementById("history-container").classList.add('hidden');
 }
 // Function for adding class
-function addClass(id){
-    document.getElementById(id).classList.add("hidden");
+function addClass(id) {
+  document.getElementById(id).classList.add("hidden");
 }
-function callBlog(){
-   //window.location.href="blog.html";
-   window.open("blog.html", "_blank");
-    
-    
+function callBlog() {
+  //window.location.href="blog.html";
+  window.open("blog.html", "_blank");
+
+
 }
 // For calling Home page
-function callHome(){
-   
-    console.log("call home");
-     window.location.href="index.html";
-    //window.open("index.html", "_blank");
+function callHome() {
+
+  //console.log("call home");
+  window.location.href = "index.html";
+  //window.open("index.html", "_blank");
 }
 
 // Receive Input from input field:
-function getInputFieldValue(id){
-    const input = document.getElementById(id).value;
-    console.log(input);
-    return input;
+function getInputFieldValue(id) {
+  const input = document.getElementById(id).value;
+  //console.log(input);
+  return input;
 }
 // Update balcnce:
 
-function updateBalance(cardValue){
+function updateBalance(cardValue) {
   const currentBalance = document.getElementById("current-balance-btn").innerText;
-  console.log(currentBalance);
+  //console.log(currentBalance);
 
-  const updateBalance =parseFloat(currentBalance)- parseFloat(cardValue);
+  const updateBalance = parseFloat(currentBalance) - parseFloat(cardValue);
   document.getElementById("current-balance-btn").innerText = updateBalance;
+ 
 }
 
+// History Added:
+function historyAdded(cardValue, location) {
+  //console.log(cardValue);
+  const div = document.createElement('div');
+  div.style.background = 'white';
+  div.style.width = '100%';
+  div.style.height = 'auto';
+  div.style.borderRadius = '10px';
+  div.style.boxShadow = 'rgba(0, 0, 0, 0.16) 0px 1px 4px'
+  div.style.alignItems = 'center';
+  div.style.marginBottom = '10px';
+  
+
+  const p = document.createElement('p');
+  p.innerText = `${cardValue} Taka is ${location} \n Date: ${new Date}`;
+
+  div.appendChild(p);
+  document.getElementById('history-container').appendChild(div);
+}
+// Open Modal :
+function openModal(){
+  document.getElementById("call-modal").classList.remove("hidden")
+  my_modal_1.showModal();
+}
